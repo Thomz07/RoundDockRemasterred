5f915d096488e3d144ddd113e8e2e23311c95acd
Thomas <thom.laffay@gmail.com>
2023-07-05 17:04:01 +0200

    New header cell (sorry Kirb)

--- rounddockremasteredpreferences/ROURootListController.h
+++ rounddockremasteredpreferences/ROURootListController.h
@@ -1,6 +1,10 @@
 #import <Preferences/PSListController.h>
+#import <Preferences/PSTableCell.h>
 #import <CepheiPrefs/HBRootListController.h>
 
 @interface ROURootListController : HBListController
 
+@end
+
+@interface RoundDockHeaderCell : PSTableCell
 @end
--- rounddockremasteredpreferences/ROURootListController.m
+++ rounddockremasteredpreferences/ROURootListController.m
@@ -14,3 +14,62 @@
 }
 
 @end
+
+@implementation RoundDockHeaderCell 
+
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(id)reuseIdentifier specifier:(id)specifier {
+	self = [super initWithStyle:style reuseIdentifier:reuseIdentifier specifier:specifier];
+
+	if (self) {
+
+		NSBundle *bundle = [[NSBundle alloc]initWithPath:@"/var/jb/Library/PreferenceBundles/RoundDockRemasteredPreferences.bundle"];
+		UIImage *logo = [UIImage imageWithContentsOfFile:[bundle pathForResource:@"icon@1080" ofType:@"png"]];
+		UIImageView *icon = [[UIImageView alloc]initWithImage:logo];
+		icon.layer.masksToBounds = YES;
+		icon.layer.cornerRadius = 16;
+		icon.translatesAutoresizingMaskIntoConstraints = NO;
+
+		UILabel *tweakLabel = [[UILabel alloc] init];
+		[tweakLabel setTextAlignment:NSTextAlignmentCenter];
+        [tweakLabel setFont:[UIFont systemFontOfSize:30 weight: UIFontWeightRegular]];
+        tweakLabel.text = @"RoundDock";
+        tweakLabel.translatesAutoresizingMaskIntoConstraints = NO;
+
+		UILabel *devLabel = [[UILabel alloc] init];
+		[devLabel setTextAlignment:NSTextAlignmentCenter];
+        [devLabel setFont:[UIFont systemFontOfSize:15 weight: UIFontWeightRegular]];
+        devLabel.text = @"By Thomz";
+        devLabel.translatesAutoresizingMaskIntoConstraints = NO;
+		devLabel.alpha = 0.8;
+
+		[self.contentView addSubview:icon];
+		[self.contentView addSubview:tweakLabel];
+		[self.contentView addSubview:devLabel];
+
+		// Add constraints
+		[NSLayoutConstraint activateConstraints:@[
+			[icon.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
+			[icon.centerYAnchor constraintEqualToAnchor:self.contentView.centerYAnchor constant:-30],
+			[icon.widthAnchor constraintEqualToConstant:100],
+			[icon.heightAnchor constraintEqualToConstant:100],
+			[tweakLabel.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
+			[tweakLabel.topAnchor constraintEqualToAnchor:icon.bottomAnchor constant:10],
+			[devLabel.centerXAnchor constraintEqualToAnchor:self.contentView.centerXAnchor],
+			[devLabel.topAnchor constraintEqualToAnchor:tweakLabel.bottomAnchor]
+		]];
+
+    }
+    	return self;
+
+}
+
+- (instancetype)initWithSpecifier:(PSSpecifier *)specifier {
+	return [self initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"CenamoHeaderCell" specifier:specifier];
+}
+
+- (void)setFrame:(CGRect)frame {
+	frame.origin.x = 0;
+	[super setFrame:frame];
+}
+
+@end
